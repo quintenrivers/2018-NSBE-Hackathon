ef51067c22494678da5b88eebe2d081fee8e896e
Larry Rentz <lrentzjr@ufl.edu>
2018-03-24 00:55:33 -0400

    added changes to registration code

--- Registration/script.js
+++ Registration/script.js
@@ -1,5 +1,12 @@
+addBusiness("Carol's Kitchen", "Food", "Gainesville", "Homestyle soul food", "412-979-3376", "10425 Sugarberry St", " ", "32612");
+addUser("carol56", "Carol's Kitchen", "Carol", "Strickland", "12345");
+addUser("carol609", "Carol's Kitchen", "Carol", "Strickland", "12345");
+
+
 function addBusiness(business, category, city, description, phone, street_address, website, zipcode)
 {
+
+
 	firebase.database().ref("/Businesses/" + business).set(
 	{
 		Category: category,
@@ -8,18 +15,18 @@ function addBusiness(business, category, city, description, phone, street_addres
 		Phone: phone,
 		StreetAdress: street_address,
 		Website: website,
-		Zipcode: zipcode,
+		Zipcode: zipcode
 	});
 }
 
 function addUser(userID, business, first_name, last_name, password)
 {
-	firebase.database().ref("/Users/" + userID).set
-	({
-		Business: business;
-		FirstName: first_name;
-		LastName: last_name;
-		Password: password;	
+	firebase.database().ref("/Users/" + userID).set(
+	{
+		Business: business,
+		FirstName: first_name,
+		LastName: last_name,
+		Password: password
 	});
 
 }
