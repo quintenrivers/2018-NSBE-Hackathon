a631884ce86c960416ed68601a5ca8fba4792010
Larry Rentz <lrentzjr@ufl.edu>
2018-03-24 02:00:06 -0400

    added user verificationand authetication functions

--- Registration/script.js
+++ Registration/script.js
@@ -1,3 +1,7 @@
+addUser("carol23", "C's Kitchen", "Carol", "Strickland", "123");
+addUser("carol23", "C's Kitchen", "Carol", "Strickland", "123");
+verifyUser("carol23");
+authenticateUser("carol23", "122");
 //adds a business to the database
 function addBusiness(business, category, city, description, phone, street_address, website, zipcode)
 {
@@ -25,5 +29,35 @@ function addUser(userID, business, first_name, last_name, password)
 		LastName: last_name,
 		Password: password
 	});
+
+}
+
+//Function verfies if usernmae is taken or not. 
+function verifyUser(userID) {
+	var dbref = firebase.database().ref('/Users/' + userID);
+	dbref.once('value').then(snap => {
+		if(snap.val() == null) {
+			//Username is available do something 
+			console.log(userID + " is available");
+		}
+		else {
+			//Username is taken do something 
+			console.log(userID + " is taken");
+		}
+	});	
+}
+
+function authenticateUser(userID, password) {
+	var dbref = firebase.database().ref('/Users/' + userID + "/password");
+	dbref.once('value').then(snap => {
+		if(snap.val() == password) {
+			//Username and password are valid. do something
+			console.log("Username or password is valid");
+		}
+		else {
+			//Username and password is not valid , do something 
+			console.log("Username or password not valid");
+		}
+	});	
 
 }
